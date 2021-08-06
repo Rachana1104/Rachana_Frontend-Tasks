df889b1e2b703eb451a5332dec17f6f09c01ffa8
Rachana1104 <87826210+Rachana1104@users.noreply.github.com>
2021-08-07 00:09:06 +0530

    Update script.js

--- Task 3/script.js
+++ Task 3/script.js
@@ -1,21 +1,22 @@
-let centers = []
-let today, d, m, y
-today = new Date()
-d = today.getDate()
-m = today.getMonth() + 1
-y = today.getFullYear()
-today = `${d}-${m}-${y}`
+let centers = [];
+const cards = document.querySelector(".cards");
+const searchBtn = document.querySelector(".searchBox").querySelector("button");
+
+let today, d, m, y;
+today = new Date();
+d = today.getDate();
+m = today.getMonth() + 1;
+y = today.getFullYear();
+today = `${d}-${m}-${y}`;
 
 function cowinData(pincode) {
     let url = `http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${today}`;
-    //console.log('cowinData');
-    let response = fetch(url)
-    fetch(url)
-        .then(response => {
-            response.json()
-                .then(data => {
-                    if (response.status === 200) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function() {
+                    if (this.status === 200) {
                         let data = JSON.parse(this.responseText);
+                        
                         if (data.sessions !== []) {
                             data.sessions.map((e, i) => {
                                 let centerInfo = [
@@ -23,6 +24,7 @@ function cowinData(pincode) {
                                     e.address,
                                     e.vaccine,
                                     e.date,
+                                    e.fee_type,
                                     e.available_capacity,
                                     e.block_name,
                                     e.block_name,
@@ -49,48 +51,66 @@ function cowinData(pincode) {
                <span class="category">Date Of Vaccination - </span>
              ${centers[i][3]}
          </h3>
+         <span class="category">Paid / Free  - </span>
+    ${centers[i][4]}
+  </h3>
+  <h3>
+    <span class="category">Minimum Age Limit - </span>
+    ${centers[i][5]}
+  </h3>
        <h3>
                   <span class="category">Available Capacity - </span>
-                ${centers[i][5]}
+                ${centers[i][6]}
             </h3>
           <h3>
           <span class="category">Block Name - </span>
-        ${centers[i][6]}
+        ${centers[i][7]}
     </h3>
   <h3>
                  <span class="category">District Name - </span>
-               ${centers[i][7]}
+               ${centers[i][8]}
            </h3>
          <h3>
          <span class="category">Available Slots - </span>
-        ${centers[i][8].join(" | ")
+        ${centers[i][9].join(" | ")
     }
    </h3>
                  </div>
                </div>`;
                                 cards.innerHTML += code;
-                                return response.json();
-                            })
+                            });
 
                             if (data.sessions.length === 0) {
-                                alert("No data Found")
+                                alert("No Vaccinations Available")
                             }
                             centers = []
-
-                            const input = document.querySelector("#input")
-                            input.addEventListener("keypress", (e) => {
-                                if (e.which === 13) {
-                                    let pincode = input.value;
-                                    cards.innerHTML = "";
-                                }
-                            })
-                        } else {
-                            alert("Some Error Occured")
                         }
-                    }
+                            } else {
+            alert("Some error occured")
+        }
+    };
 
-                })
-
-        })
+    xhr.send();
 }
-cowinData();
+
+function repeat() {
+    let pincode = input.value;
+    cards.innerHTML = "";
+    if (pincode === "") {
+        alert("Enter pincode in the search box")
+    } else if (pincode !== "") {
+        cowinData(pincode)
+        document.getElementById('input').value = "";
+    }
+}
+
+const input = document.querySelector("#input")
+input.addEventListener("keypress", (e) => {
+    if (e.which === 13) {
+        repeat()
+    }
+})
+
+searchBtn.addEventListener("click", () => {
+    repeat()
+})
